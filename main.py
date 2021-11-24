1bf6a193a657f914a9c1a4ac8a2d6596b2122dca
egor_go_go <chukovskiy.egorr@yandex.ru>
2021-11-24 16:43:35 +0300

    Create yellow circles

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')
-    print('NO')
+import sys
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QWidget
+from random import choice
 
 
-# Press the green button in the gutter to run the script.
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.setWindowTitle('Записная книжка')
+        self.pushButton.clicked.connect(self.onClicked)
+        self.flag = False
+
+    def onClicked(self):
+        self.flag = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setPen(QColor('yellow'))
+            qp.drawEllipse(self.random_size(), self.random_size(), self.random_size(), self.random_size())
+
+    def random_size(self):
+        size = [i for i in range(300)]
+        return choice(size)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = Window()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
